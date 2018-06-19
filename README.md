222adc38635cd2ba32e0ffa6fe403d9e9c654ea5
Chin <justinfchin@gmail.com>
2018-06-19 17:58:16 -0400

    updated readme and added test file

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # 2018spotifyfellowship
+- Each solution contains the function requested, a test function, and a main function. 
+    - to run, you can:
+        1. uncomment the test function call in the main method
+        2. or two compile and run the test file which calls the test function for each question
+            - `javac test.java`
+            - `java test`
 
 ## Question 1 
 -- sortByStrings(s,t): Sort the letters in the string s by the order they occur in the string t. You can assume t will not have repetitive characters. For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw". For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
---
+++ test.java
@@ -0,0 +1,27 @@
+// Test Class for Testing the function solution written for the 3 questions
+
+class test{
+
+    public static void main(String[] args){
+        // Question 1
+        System.out.println("\n============");
+        System.out.println("Question 1:");
+        System.out.println("============");
+        q1.test();
+
+        // Question 2
+        System.out.println("\n============");
+        System.out.println("Question 2:");
+        System.out.println("============");
+        q2.test();
+
+        // Question 3
+        System.out.println("\n============");
+        System.out.println("Question 3:");
+        System.out.println("============");
+        q3.test();
+
+
+    }
+    
+}
